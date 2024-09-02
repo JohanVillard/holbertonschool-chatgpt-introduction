322053f12ef4efc6110af8d83419d0d9c84f2249
JohanVillard <styx.locke@gmail.com>
2024-09-02 12:46:21 +0200

    Apply NumPy style

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -4,16 +4,22 @@ import sys
 
 def factorial(n):
     """
-    Compute the factorial of n.
+    Compute the factorial of a non-negative integer n using recursion.
 
-    Function Description: this function calculates the factorial of a non-negative integer n using recursion.
     The factorial of a number n (denoted as n!) is the product of all positive integers
     less than or equal to n. For example, 5! = 5 * 4 * 3 * 2 * 1 = 120.
 
-    Parameters n: a non-negative integer whose factorial is to be computed.
-        Must be an integer greater than or equal to 0.
+    Parameters
+    ----------
+    n : int
+        A non-negative integer whose factorial is to be computed. Must be an integer
+        greater than or equal to 0.
 
-    Returns: the factorial of the input integer n. If n is 0, returns 1.
+    Returns
+    -------
+    int
+        The factorial of the input integer n. If n is 0, returns 1. For example,
+        factorial(4) returns 24, and factorial(0) returns 1.
     """
     # Base case: if n is 0, return 1. This is because the factorial of 0 is defined to be 1.
     if n == 0:
